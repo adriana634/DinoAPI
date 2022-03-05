da4703b8c44f3c27222ae38603a57abeb4874d13
adriana634 <50183872+adriana634@users.noreply.github.com>
2022-03-05 21:51:10 +0100

    DinoWikipedia script

--- DinoWikipedia/DinoWikipedia.py
+++ DinoWikipedia/DinoWikipedia.py
@@ -1,1 +1,11 @@
+import re
 
+with open('List_of_dinosaur_genera.xml', mode='r', encoding='utf-8') as dinosaur_xml:
+    dinosaur_xml_content = dinosaur_xml.read()
+    
+genera_pattern = re.compile(r'\* (?:"|'')\[\[([A-Z][a-z]+)\]\](?:"|'')')
+dinosaur_genera_list = genera_pattern.findall(dinosaur_xml_content)
+dinosaur_genera_joined = ',\n'.join(dinosaur_genera_list)
+
+with open('List_of_dinosaur_genera.output', mode='w', encoding='utf-8') as dinosaur_output:
+    dinosaur_output.write(dinosaur_genera_joined)
