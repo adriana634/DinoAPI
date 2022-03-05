0774e4c38a27c9b3ee61596216dc7fed9f341d67
adriana634 <50183872+adriana634@users.noreply.github.com>
2022-03-05 22:12:07 +0100

    Fix dinosaur genera regular expression metacharacters escaping

--- DinoWikipedia/DinoWikipedia.py
+++ DinoWikipedia/DinoWikipedia.py
@@ -3,7 +3,7 @@ import re
 with open('List_of_dinosaur_genera.xml', mode='r', encoding='utf-8') as dinosaur_xml:
     dinosaur_xml_content = dinosaur_xml.read()
     
-genera_pattern = re.compile(r'\* (?:"|'')\[\[([A-Z][a-z]+)\]\](?:"|'')')
+genera_pattern = re.compile(r'\* (?:"|\'\')\[\[([A-Z][a-z]+)\]\](?:"|\'\')')
 dinosaur_genera_list = genera_pattern.findall(dinosaur_xml_content)
 dinosaur_genera_joined = ',\n'.join(dinosaur_genera_list)
 
